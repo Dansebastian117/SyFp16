adc0bc9af72bad297c138a120933fede620c914c
administrador1 <administrador1@R_22>
2016-05-31 23:40:50 -0500

    mi primer intento de cifrado

---
+++ SyPF16/src/E1/Cesar.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package E1;
+
+/**
+ *
+ * @author administrador1
+ */
+public class Cesar {
+    public String cifrar (String mensaje, int constante){
+        String resultado= "";
+        for (int i = 0; i<mensaje.length(); i++){
+
+            resultado = resultado+(char)(mensaje.charAt(i)+ constante);
+        }
+        return resultado;
+        
+    }
+    
+    public String Dcifrar (String m1, int n1){
+        
+        String resultado= "";
+        for (int i = 0; i<m1.length(); i++){
+
+            resultado = resultado+(char)(m1.charAt(i)- n1);
+        }
+        return resultado;
+    }
+    
+    
+    public static void main(String[]args){
+        Cesar c = new Cesar();
+        String s= c.cifrar("hola", 2);
+        System.out.println(s);
+        String s1=c.Dcifrar(s, 2);
+        System.out.println(s1);
+        
+        
+    }
+    
+}
